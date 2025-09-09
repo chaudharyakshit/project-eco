876175e0319c6f9d0c42a0b85a3905d9fe190eb3
Akshit Chaudhary <140873718+chaudharyakshit@users.noreply.github.com>
2025-09-09 12:30:07 +0530

    Updated backend: server.js, package.json, package-lock.json

--- server/server.js
+++ server/server.js
@@ -11,11 +11,12 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",  // removed extra space
+  origin: ["http://localhost:5173", "https://ecocruze.in"], // array of allowed origins
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
+
 app.use(express.json());
 
 // Routes
