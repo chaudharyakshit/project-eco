f4022253e66831b071fc959e7e1dcb443e43b48c
Akshit Chaudhary <140873718+chaudharyakshit@users.noreply.github.com>
2025-09-17 12:48:00 +0530

    Update client HTML/CSS and component files

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,19 @@
 // components/Header/Header.jsx
-import React from 'react';
-import { Link } from 'react-router-dom'; 
+import React, { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom'; 
 import './Header.css';
 import { FaSearch, FaUser, FaShoppingBag, FaBars } from 'react-icons/fa';
 import logo from '../assets/main-logo.png';
 
 const Header = ({ scrolled }) => {
   const [mobileMenuOpen, setMobileMenuOpen] = React.useState(false);
+  const location = useLocation();
+
+  // Auto-close mobile menu on route change
+  useEffect(() => {
+    if (mobileMenuOpen) setMobileMenuOpen(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.pathname]);
 
   return (
     <header className={scrolled ? 'scrolled' : ''}>
@@ -16,13 +23,12 @@ const Header = ({ scrolled }) => {
         </Link>
         
         <nav className={mobileMenuOpen ? 'mobile-open' : ''}>
-          <ul>
+          <ul onClick={() => setMobileMenuOpen(false)}>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About Us</Link></li>
             <li><Link to="/services">Services</Link></li>
             <li><Link to="/contact">Contact Us</Link></li>
             <li><Link to="/dealership">Dealership</Link></li>
-
             <li><Link to="/news">News</Link></li>
           </ul>
         </nav>
